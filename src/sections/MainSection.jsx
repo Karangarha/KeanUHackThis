27e7d4f91e3b3ad3c652abfc7257a7e28b0a61a5
Karangarha <garhasaab001@gmail.com>
2026-03-05 15:29:14 -0500

    updated the content

--- src/sections/MainSection.jsx
+++ src/sections/MainSection.jsx
@@ -1,5 +1,74 @@
+import { motion } from "motion/react";
+import TextType from "../assets/TextType";
+import CountdownTimer from "../assets/CountDownTime";
+
+const glowAnimation = {
+  animate: {
+    textShadow: [
+      "0 0 5px #fff",
+      "0 0 10px #fff",
+      "0 0 20px #82b1ff",
+      "0 0 10px #82b1ff",
+      "0 0 5px #fff",
+    ],
+  },
+  transition: {
+    duration: 2,
+    repeat: Infinity,
+    repeatType: "reverse",
+  },
+};
+
 const MainSection = () => {
-  return <section></section>;
+  return (
+    <section id="home" className="h-screen w-full">
+      <div className="flex flex-col justify-center items-center h-full text-white gap-1">
+        <motion.div {...glowAnimation}>
+          <TextType
+            text="KeanUHackThis"
+            as="h1"
+            typingSpeed={70}
+            initialDelay={0}
+            pauseDuration={2000}
+            deletingSpeed={30}
+            loop={false}
+            className="text-8xl font-bold"
+            showCursor={false}
+            hideCursorWhileTyping={true}
+            cursorCharacter="|"
+            cursorClassName=""
+            cursorBlinkDuration={0.5}
+            textColors={[]}
+            variableSpeed={null}
+            onSentenceComplete={null}
+            startOnVisible={false}
+            reverseMode={false}
+          />
+        </motion.div>
+        <div>
+          <h3 className="text-4xl font-bold">24Hour Hackathon Spring 2026</h3>
+        </div>
+        <div>
+          <p className="text-xl font-bold">
+            April 25-26, Kean University, Union, New Jersey
+          </p>
+        </div>
+        <div className="mt-10">
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ duration: 0.2 }}
+            className="text-4xl font-bold border border-white rounded-full px-8 py-2 hover:bg-white/10 hover:backdrop-blur-sm transition-all duration-200"
+          >
+            Register Now
+          </motion.button>
+        </div>
+        <div className="mt-1">
+          <CountdownTimer targetDate="2026-04-25" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default MainSection;
