a37d425efc790eb793c9f9bd536699d7f4dc84a8
Karangarha <garhasaab001@gmail.com>
2026-03-05 14:50:02 -0500

    feat: implement animated navbar with dock component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,59 @@
+import {
+  Home,
+  Handshake,
+  Lightbulb,
+  Calendar,
+  User,
+  CircleQuestionMark,
+  Mail,
+} from "lucide-react";
+import Dock from "../assets/Dock";
+
 const Navbar = () => {
-  return <nav></nav>;
+  const navLinks = [
+    { icon: <Home size={24} color="white" />, label: "Home", href: "#home" },
+    {
+      icon: <Handshake size={24} color="white" />,
+      label: "Sponser",
+      href: "#sponser",
+    },
+    {
+      icon: <Lightbulb size={24} color="white" />,
+      label: "Theme",
+      href: "#theme",
+    },
+    {
+      icon: <Calendar size={24} color="white" />,
+      label: "Schedule",
+      href: "#schedule",
+    },
+    {
+      icon: <User size={24} color="white" />,
+      label: "Alumini",
+      href: "#alumini",
+    },
+    {
+      icon: <CircleQuestionMark size={24} color="white" />,
+      label: "FAQ",
+      href: "#faq",
+    },
+    {
+      icon: <Mail size={24} color="white" />,
+      label: "Contact",
+      href: "#contact",
+    },
+  ];
+
+  return (
+    <nav className="flex justify-center fixed w-full">
+      <Dock
+        items={navLinks}
+        panelHeight={68}
+        baseItemSize={50}
+        magnification={70}
+      />
+    </nav>
+  );
 };
 
 export default Navbar;
